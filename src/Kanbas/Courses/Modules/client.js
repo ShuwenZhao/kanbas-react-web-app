7ccfe63da7cb01be6641dbc78b2f58ae76757ee3
Shuwen Zhao <shuwenzhao@yahoo.com>
2023-11-15 10:16:05 -0800

    modify the server URLs

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-const COURSES_URL = "https://kanbas-node-server-app-3mmp.onrender.com/api/courses";
-const MODULES_URL = "https://kanbas-node-server-app-3mmp.onrender.com/api/modules";
+const API_BASE = process.env.REACT_APP_API_BASE;
+const MODULES_URL = `${API_BASE}/modules`;
+const COURSES_URL = `${API_BASE}/courses`
 
 export const updateModule = async (module) => {
   const response = await axios.put(`${MODULES_URL}/${module._id}`, module);
