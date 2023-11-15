9120964b9031878f5410abc7e3bf45981a77443b
Shuwen Zhao <shuwenzhao@yahoo.com>
2023-11-15 10:36:08 -0800

    modify courses url

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -14,7 +14,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function Courses() {
-  const URL = "http://localhost:4000/api/courses";
+  const API_BASE = process.env.REACT_APP_API_BASE;
+  const URL = `${API_BASE}/courses`
   const { courseId } = useParams();
   const [course, setCourse] = useState({});
   const findCourseById = async (courseId) => {
