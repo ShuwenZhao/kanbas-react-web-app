9dd39e5ecf1055ea7628836c56e399440b30d4a3
Shuwen Zhao <shuwenzhao@yahoo.com>
2023-11-18 22:58:15 -0800

    modify Labs/a5

--- src/Labs/a5/WorkingWithObjects.js
+++ src/Labs/a5/WorkingWithObjects.js
@@ -31,14 +31,14 @@ function WorkingWithObjects() {
       <h3>Working With Objects</h3>
       <h4>Retrieving Objects</h4>
       <a
-        href="http://localhost:/a5/assignment"
+        href={`${URL}/assignment`}
         className="btn btn-primary me-2"
       >
         Get Assignment
       </a>
       <h4>Retrieving Properties</h4>
       <a
-        href="http://localhost:4000/a5/assignment/title"
+        href={`${URL}/assignment/title`}
         className="btn btn-primary me-2"
       >
         Get Title
