612d57ebaa06cae97d92b1a3bde527005c5a99f2
Shuwen Zhao <shuwenzhao@yahoo.com>
2023-11-23 15:58:12 -0800

    a6 first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Labs from "./Labs";
 import HelloWorld from "./Labs/a3/HelloWorld";
 import Kanbas from "./Kanbas";
+import Project from "./Project";
 import { HashRouter } from "react-router-dom";
 import { Routes, Route, Navigate } from "react-router";
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/Labs" />} />
           <Route path="/hello" element={<HelloWorld />} />
+          <Route path="/Project/*" element={<Project />} />
           <Route path="/Labs/*" element={<Labs />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
---
+++ src/Project/index.js
@@ -0,0 +1,55 @@
+import Home from "./home";
+import Login from "./login";
+import Signup from "./users/signup";
+import Profile from "./profile";
+import Search from "./search";
+import Details from "./details";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
+import { useState } from "react";
+import UserList from "./users/list";
+import SignIn from "./users/signin";
+import Account from "./users/account";
+import UserTable from "./users/table";
+function Project() {
+  const { pathname } = useLocation();
+  return (
+    <div className="row">
+      <h1>Project</h1>
+      <div className="row">
+        <div className="col-2">
+          <div className="list-group">
+            <Link to="/Project/" className={`list-group-item nav-link ${pathname === "/Project/" ? "active" : ""}`}>
+              Home
+            </Link>
+            <Link to="/Project/signin" className={`list-group-item nav-link ${pathname === "/Project/signin" ? "active" : ""}`}>
+              Signin
+            </Link>
+            <Link to="/Project/signup" className={`list-group-item nav-link ${pathname === "/Project/signup" ? "active" : ""}`}>
+              Signup
+            </Link>
+            <Link to="/Project/account" className={`list-group-item nav-link ${pathname === "/Project/account" ? "active" : ""}`}>
+              Account
+            </Link>
+            <Link to="/Project/search" className={`list-group-item nav-link ${pathname === "/Project/search" ? "active" : ""}`}>
+              Search
+            </Link>
+          </div>
+        </div>
+        <div className="col-10">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/search/:search" element={<Search />} />
+            <Route path="/details/:albumId" element={<Details />} />
+            <Route path="/users" element={<UserList />} />
+            <Route path="/users/table" element={<UserTable />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Project;
